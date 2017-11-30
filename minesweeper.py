4235f8c7b8dda50ef47b554186c39bd0c55a18fb
Levente Belteky <belteky.levente@gmail.com>
2017-11-30 14:46:38 +0100

    feat(app): add mines to the grid
    and also fix the quotation marks.

--- minesweeper.py
+++ minesweeper.py
@@ -1,33 +1,45 @@
 import os
+import random
 
 SIZE = 10
 
 def init_map(size):
     return [[{'isMine': False, 'isChecked': False} for _ in range(SIZE)] for _ in range(SIZE)]
 
+def init_mines(grid, max_mines):
+    row = random.randint(0, len(grid) - 1)
+    col = random.randint(0, len(grid) - 1)
+    print(row, col)
+    grid[row][col]['isMine'] = True
+
 def draw(grid):
     for row in grid:
       for field in row:
-        print("[ ]" if field['isChecked'] else "[?]", end="")
+        print('[ ]' if field['isChecked'] else '[?]', end='')
       print()
 
 def check(grid, row, col):
     grid[row][col]['isChecked'] = True
+    if grid[row][col]['isMine']:
+        return False
+    return True
 
 clear = lambda: os.system('clear')
 
 def get_row_and_col():
-    fieldToCheck = input("Which field: ")
+    fieldToCheck = input('Which field: ')
     row, col = fieldToCheck.split()
     row, col = int(row) - 1, int(col) - 1
     return row, col
 
 def start_game():
     grid = init_map(SIZE)
+    init_mines(grid, 10)
     draw(grid)
-    while True:
+    is_live = True
+    while is_live:
       row, col = get_row_and_col()
-      check(grid, row, col)
+      is_live = check(grid, row, col)
       clear()
       draw(grid)
 
